67ae102285820ee30bfd73b24a41db934d968be7
Mikhail Azeev <azv-m@outlook.com>
2022-07-31 14:39:37 +0300

    Adjust formatting, update libs

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # GLaDe
+
 Game Language Decoder
 
 [Try it out](https://azvm.github.io/GLaDe)
--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 const app = new Vue({
-    el: "#app",
-    data() {
-        return {
-            currentLanguageIndex: 0,
-            encodedText: "A QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
-        }
+  el: "#app",
+  data() {
+    return {
+      currentLanguageIndex: 0,
+      encodedText: "A QUICK BROWN FOX JUMPS OVER THE LAZY DOG",
+    };
+  },
+  computed: {
+    currentLanguage() {
+      return LANGUAGES[this.currentLanguageIndex];
     },
-    computed: {
-        currentLanguage() {
-            return LANGUAGES[this.currentLanguageIndex]
-        },
-        decodedText() {
-            return this.currentLanguage.decode(this.encodedText)
-        },
-        fontStyle() {
-            return {
-                "font-family": this.currentLanguage.fontFamily,
-            }
-        }
-    }
-})
+    decodedText() {
+      return this.currentLanguage.decode(this.encodedText);
+    },
+    fontStyle() {
+      return {
+        "font-family": this.currentLanguage.fontFamily,
+      };
+    },
+  },
+});
